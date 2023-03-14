94a0688820b419ecaa2c050e5942543fdb7fbe26
Roman <roman.qwonix@gmail.com>
2023-03-14 13:58:35 +0300

    add Dish for data binding

--- app/src/main/java/ru/qwonix/android/foxwhiskers/Dish.kt
+++ app/src/main/java/ru/qwonix/android/foxwhiskers/Dish.kt
@@ -1,4 +1,9 @@
 package ru.qwonix.android.foxwhiskers
 
-class Dish {
-}
+class Dish(
+    var title: String,
+    var imageUrl: String,
+    var shortDescription: String,
+    var currencyPrice: String,
+    var type: String
+)
