54b0d5b020acd64e2c8db5ab6587c99b6298770d
Roman <roman.qwonix@gmail.com>
2023-03-17 13:24:24 +0300

    add OrderFragment, ProfileFragment and navigation

--- app/src/main/java/ru/qwonix/android/foxwhiskers/MainActivity.kt
+++ app/src/main/java/ru/qwonix/android/foxwhiskers/MainActivity.kt
@@ -1,11 +1,21 @@
 package ru.qwonix.android.foxwhiskers
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_main) as NavHostFragment
+
+        findViewById<BottomNavigationView>(R.id.bottom_navigation_view_main).setupWithNavController(
+            navHostFragment.navController
+        )
     }
 }
